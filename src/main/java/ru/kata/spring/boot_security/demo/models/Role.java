150794c0150b1ad7ccc119344b530e4c22c46706
max.popovv <max.popovv@gmail.com>
2024-07-23 07:34:38 +0300

    first commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
@@ -0,0 +1,75 @@
+package ru.kata.spring.boot_security.demo.models;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "role")
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    public Role() {
+    }
+
+    public Role(Long id) {
+        this.id = id;
+    }
+
+    public Role(Long id, String role) {
+        this.id = id;
+        this.roleName = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return roleName;
+    }
+
+    public void setRole(String role) {
+        this.roleName = role;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role1 = (Role) o;
+        return Objects.equals(id, role1.id) && Objects.equals(roleName, role1.roleName) && Objects.equals(users, role1.users);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, roleName, users);
+    }
+
+    @Override
+    public String toString() {
+        return roleName;
+    }
+}
